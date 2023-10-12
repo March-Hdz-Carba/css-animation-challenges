da3e5c11e8c5d86bd6f9baff59909a312fa29df6
March-hdz-Carba <marcela.hdez.1801@gmail.com>
2023-10-11 18:08:09 -0600

    IntersectionObserve

---
+++ Challenges/scroll-animation/index.js
@@ -0,0 +1,32 @@
+/*
+DOCUMENTATION IntersectionObserver
+Articles:
+https://blog.webdevsimplified.com/2022-01/intersection-observer/
+
+Videos:
+https://www.youtube.com/watch?v=2IbRtjez6ag
+*/
+
+/*
+threshold -> Percentage of the element must be visible before isIntersecting becomes true.
+*/
+
+const options = {
+  threshold: 1,
+  rootMargin: "-100px"
+};
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const intersecting = entry.isIntersecting;
+
+    entry.target.classList.toggle('square-animation', intersecting);
+  })
+}, options);
+
+document.addEventListener("DOMContentLoaded", function(){
+  const squares = document.querySelectorAll(".square");
+  squares.forEach(square => {
+      observer.observe(square);
+    })
+});
